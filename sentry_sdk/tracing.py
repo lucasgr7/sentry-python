b7484f3ff20355cb1b971583a7a8c13e1d9d9a30
Katie Byers <lobsterkatie@gmail.com>
2021-01-12 22:37:50 -0800

    pass sentry tracestate in event.contexts.trace for extraction when envelope is created

--- sentry_sdk/tracing.py
+++ sentry_sdk/tracing.py
@@ -418,6 +418,9 @@ class Span(object):
         if self.status:
             rv["status"] = self.status
 
+        if self._containing_transaction:
+            rv["tracestate"] = self._containing_transaction._sentry_tracestate_value
+
         return rv
 
 
